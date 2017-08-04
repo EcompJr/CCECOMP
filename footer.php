2694431a3b6052e8025a89d7817c6f45127dbdd8
Maria Luisa <luisa.fsales@gmail.com>
2017-08-04 17:16:47 -0300

    Feito o header e footer, e adicionados na pagina do mercado

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; EcompJr 2017</p>
+                </div>
+            </div>
+        </footer>
