608aca7435f680dacbc9615a24250cea0aea7591
Maria Luisa <luisa.fsales@gmail.com>
2017-08-08 17:35:07 -0300

    css do footer arrumado

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 
         <!-- Footer -->
         <footer>
+          <div class = "container">
             <div class="row">
                 <div class="col-lg-12">
                     <p>Copyright &copy; EcompJr 2017</p>
                 </div>
             </div>
+          </div>
         </footer>
