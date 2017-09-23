5796b8c6a817be6c75e1b09187a3da20c7c4ebca
Bruno Vogel <bugvogel@gmail.com>
2017-09-22 21:08:52 -0300

    Finish Ecompjr Page

--- public_html/ecompjr.php
+++ public_html/ecompjr.php
@@ -55,15 +55,67 @@
 
         <!-- Content Row -->
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-md-6">
+              <br>
+                 <h3>Quem Somos ?</h3>
 
+                 <p>Empresa Júnior de Engenharia de Computação, composta por alunos do curso da Universidade Estadual
+                   de Feira de Santana, que promove impactos positivos em micro e pequenas
+                   empresas por meio de soluções tecnológicas. Fundada em 2007, a EcompJr atualmente participa ativamente do propósito que envolve ser Movimento Empresa Júnior. A empresa promove uma vivência empresarial estratégica
+                   para estudantes interessados em liderar a mudança no atual cenário brasileiro.</p>
+                   <br>
+                   <div class="row">
+                     <div class="col-md-12">
+                       <h3>Junte-se a nós</h3>
+                       <p>Para fazer parte da EcompJr, o estudante deve se inscrever no processo seletivo enquanto estiver aberto, normalmente no início de cada semestre.
+                           O processo seletivo conta com uma dinâmica de grupo para avaliar soft skills e uma entrevista para avaliar o perfil do candidato a vaga.
+                          Quando selecionado, o estudante torna-se trainee da empresa, recebendo treinamento técnico e administrativo específico para a empresa</p>
+                     </div>
+                   </div>
             </div>
-            <!-- /.col-lg-12 -->
+
+              <div class="col-md-6">
+                <img width="550"alt="imagem da empresa"src="images/EcompJr-picture-group.jpg" style="border-radius:10px"     />
+              </div>
+
         </div>
-        <!-- /.row -->
+        <br>
+        <div class="row">
+          <div class="col-md-6">
+            <h3> Localização  </h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3899.7034536019305!2d-38.97561768575755!3d-12.200569991364967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7143889364b821b%3A0xae5e88b833ba28f3!2sEcompJr!5e0!3m2!1spt-BR!2sbr!4v1506102610559" width="500" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+
+          <div class="col-md-6">
+            <h3>Redes Sociais e links</h3>
+            <div class="row">
+              <div class="col-md-2">
+                <a href="https://www.facebook.com/ecompjr.uefs" target="_blank"><i  class="fa fa-facebook-square fa-3x"></i></a>
+              </div>
+              <div class="col-md-2">
+                <a target="_blank" href="http://www.ecompjr.com.br/"><img height="45" width='45'  style="border-radius:20px;"  src='images/logoEcompJr.png'/> </a>
+              </div>
+              <div class="col-md-2">
+                <a href="https://www.instagram.com/ecompjr/" target="_blank"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+
+              </div>
+              <div class="col-md-1">
+                <a role="button"  data-toggle="collapse" data-target="#info"  ><i style="color:#f0ad4e" class="fa-2x glyphicon glyphicon-plus"></i></a>
+                <div id="info" class="collapse container">
+                  <p>Email: ecompjr@uefs.br<br>
+                     Número: (75) 3161-8354
+                  </p>
+                </div>
+              </div>
+            </div>
+
+        </div>
+
 
         <hr>
     </div>
+
+  </div>
     <!-- /.container -->
 
     <!-- jQuery -->
@@ -71,6 +123,7 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+    <br>
 
     <?php require_once("footer.php") ?>
 
--- public_html/footer.php
+++ public_html/footer.php
@@ -4,7 +4,7 @@
 
             <div style="height:100px;background-color:#4d6aa0;bottom:0;width:100%;"  class="row">
                 <div  class="col-lg-12">
-                    <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"> &nbsp&nbsp <a href="http://www.ecompjr.com.br/"><img height="35" width='35'  style="border-radius:20px;"  src='images/logoEcompJr.png'/> </a>Desenvolvido por EcompJr<br></p>
+                    <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"> &nbsp&nbsp <a target="_blank" href="http://www.ecompjr.com.br/"><img height="35" width='35'  style="border-radius:20px;"  src='images/logoEcompJr.png'/> </a>Desenvolvido por EcompJr<br></p>
                 </div>
 
           </div>
