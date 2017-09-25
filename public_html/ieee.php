a687355fa031d8f81910eb73e84ad3fc0c05c2d2
Saulo Araujo <skdtarso@gmail.com>
2017-09-25 14:38:37 -0300

    Add IEEE page

--- public_html/ieee.php
+++ public_html/ieee.php
@@ -54,24 +54,55 @@
 
         <!-- Content Row -->
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-md-12">
+                <p align="justify">
+                    <br>
+                    <h3>Quem Somos ?</h3>
 
+                    O Ramo Estudantil IEEE UEFS é uma organização formada por estudantes e voluntários e membros do IEEE. Buscando sempre fomentar
+                    o avanço da tecnologia em prol da humanidade. <br><br>
+
+                    <h3>O que fazemos?</h3>
+                    No ramo IEEE UEFS, atualmente temos as seguintes atividades: <br><br>
+                    <b>Liga de Sistemas embarcados:</b> Focada no desenvolvimento e estudo de projetos envolvendo sistemas
+                    embarcados e róbotica.<br> <b>Liga de desenvolvimento de Games:</b> Focada no estudo de técnicas e desenvolvimentos
+                    de jogos digitais que agreguem valor a comunidade em geral.<br><br>
             </div>
-            <!-- /.col-lg-12 -->
-        </div>
-        <!-- /.row -->
 
-        <hr>
+            <div class="col-md-6">
+            <h3>Responsáveis:</h3>
+                    Presidente do Ramo : Victor Figueiredo<br> Vice presidente: Antonio Crispim<br> Conselheiro:
+                    <a target="_blank" href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4760205A7"> Anfranserai Morais Dias</a><br>
+                    <br>
+            </div>
+            <div class="col-md-6">
+                    <h3>Redes Sociais e links</h3>
+                    <div class="row">
+                        <div class="col-lg-2">
+                            <a href="https://www.facebook.com/RamoIEEE.UEFS/" target="_blank"><i  class="fa fa-facebook-square fa-3x"></i></a>
+                        </div>
+                    </div>
+            </div>        
+
+        </div>
+
+            </p>
+
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+
+    <hr>
 
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="js/jquery.js "></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js "></script>
 
-    <?php require_once("footer.php") ?>
+    <?php require_once("footer.php ") ?>
 </body>
 
 </html>
