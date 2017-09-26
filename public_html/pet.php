3e40ad4df581461dc64c3a228b3efddd450319a8
luanvicttor <luanvictortb@gmail.com>
2017-09-25 23:37:45 -0300

    Finish PET Page

--- public_html/pet.php
+++ public_html/pet.php
@@ -35,7 +35,6 @@
 <?php require_once("header.php") ?>
 
 <body>
-
     <!-- Page Content -->
     <div class="container">
 
@@ -54,12 +53,35 @@
         <!-- /.row -->
 
         <!-- Content Row -->
-        <div class="row">
-            <div class="col-lg-12">
+        <div class="col-md-12">
+                <br>
+                <h3>Quem Somos ?</h3>
 
-            </div>
-            <!-- /.col-lg-12 -->
+                <p align="justify">O Programa de Educação Tutorial (PET) é um programa nacional, mantido pelo ministério da Educação (MEC), sob a responsabilidade administrativa da Secretaria de Educação Superior (SESu). O PET Engenharias da UEFS consiste em um grupo formado por até 12 (doze) estudantes bolsistas e até 6 (seis) estudantes não bolsistas, que contam com a orientação de um professor(a) tutor(a), visando proporcionar condições para a realização de atividades que favoreçam a formação acadêmica integral de cada membro, bem como colaborar ao máximo para a melhoria do ensino nos cursos de graduação relacionados com o Grupo, e na universidade como um todo. <br> Instalado em 2011 como grupo dos cursos de Engenharia de Alimentos e de Computação, o grupo PET em Engenharias em decorrência das necessidades em suas atividades, abriu-se, em 2013, para o curso de Engenharia Civil e, em 2014, para o curso de Agronomia. Este grupo é o quarto dos grupos do Programa de Educação Tutorial do MEC na Universidade Estadual de Feira de Santana, que vêm sendo instituídos a partir do segundo semestre de 2007. Atualmente, a UEFS possui outros 2 (dois) grupos PET além deste, um vinculado ao curso de Ciências Econômicas e o outro ao de Odontologia. O proponente e tutor deste grupo é o professor Pablo Rodrigo Fica Piras.
+                <br>
+                Fonte:
+                <A href="http://www.seulink.com.br" title="PET ENGENHARIAS UEFS">PET ENGENHARIAS UEFS</A>
+
+                </p><br>
+
+                <h3>O que fazemos?</h3>
+                <p align="justify">Cada membro do grupo desenvolve indissociavelmente atividades de ensino, pesquisa e extensão, que propiciem uma formação com padrão cientifico, técnico, cultural e ético, nas áreas de conhecimento relacionadas com as engenharias, O Programa é institucionalizado pela lei nº 11.180/2005 e regulamentado pela portaria MEC nº 976/2010, republicada em 31 de Outubro de 2013, e pelo disposto do Manual de Orientações Básicas do Programa (2006)</p><br><br>
+
+                <h3>Redes Sociais e links</h3>
+
+                  <div class="col-md-1">
+                    <a href="https://www.facebook.com/petengsuefs/" target="_blank"><i  class="fa fa-facebook-square fa-3x"></i></a>
+                  </div>
+                  <div class="col-md-1">
+                    <a target="_blank" href="http://www.petengsuefs.net"><img height="45" width='45'  style="border-radius:20px;"  src='images/logoPET.png'/> </a>
+                  </div>
+                  <div class="col-md-1">
+                    <a href="https://www.instagram.com/pet_engs_uefs/" target="_blank"><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+                  </div>
+                  <br><br><br>
+
         </div>
+
         <!-- /.row -->
 
         <hr>
