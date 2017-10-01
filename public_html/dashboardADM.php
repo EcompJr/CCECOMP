b43d55fe5ddfefbbc135c31317b6ba8757a1b909
Bruno Vogel <bugvogel@gmail.com>
2017-09-30 23:29:50 -0300

    Model ADM Page
    Finish nav-bar ADM page

---
+++ public_html/dashboardADM.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>CCECOMP UEFS</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link href="css/navbarADM.css" rel="stylesheet" >
+
+    <!-- Favicon -->
+    <link rel="icon" type="images/png" sizes="32x32" href="images/favicon.ico">
+
+
+
+  </head>
+  <body>
+
+       <?php require_once("navBarADM.php")?>
+
+    <div class="row">
+      <div class="col-md-offset-4 col-md-7">
+        <h1>ADMIN PAGE</h1>
+        <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed porttitor lacus. Cras in quam aliquam mi auctor blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin euismod magna diam, sed mattis tortor auctor non. Curabitur pretium molestie congue. In vel erat aliquet, porttitor felis nec, maximus lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id arcu quam. Nunc tincidunt ipsum vel faucibus maximus. Ut pharetra sapien lacinia euismod rutrum.
+
+Integer tincidunt ut leo non imperdiet. Nulla facilisi. Vivamus ac tellus et neque imperdiet convallis ac ut ligula. Aliquam ac lorem id est efficitur efficitur vel ac nisi. Quisque quis auctor sapien. In nec tortor tincidunt, lobortis quam in, eleifend quam. Quisque ac faucibus sapien, non laoreet arcu. Pellentesque elit sem, interdum in pharetra ut, malesuada vitae tortor. Ut viverra ipsum nibh, ac semper mi congue vel. Fusce tempus non quam eu euismod. Curabitur volutpat nisl tristique enim fringilla semper. Vivamus convallis tellus at ligula porttitor bibendum. Aenean dolor quam, lobortis tristique sollicitudin quis, congue a risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed ligula non ipsum blandit mattis at sed arcu. Vestibulum congue ipsum mollis tortor pretium, id tincidunt leo aliquam.
+        </p>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/navbarADM.js" > </script>
+       <br><br><br>
+   <?php require_once("footer.php") ?>
+
+  </body>
+</html>
---
+++ public_html/navBarADM.php
@@ -0,0 +1,55 @@
+
+<div id="wrapper">
+       <div class="overlay"></div>
+
+       <!-- Sidebar -->
+       <nav class="navbar navbar-inverse  navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+           <ul class="nav sidebar-nav">
+               <li class="sidebar-brand">
+                   <a href="#">
+                      <img  alt="logo do colegiado" width="40"  src="images/customLogo1.png" /> CCECOMP ADM
+
+                   </a>
+               </li>  <br><br>
+               <li>
+                   <a href="#">Home</a>
+               </li>
+               <li>
+                 <a href="#">Notícias</a>
+               </li>
+               <li>
+                   <a href="#">TCC</a>
+               </li>
+               <li>
+                   <a href="#">Resoluções</a>
+               </li>
+               <li>
+                   <a href="#">Oportunidades de Estágio</a>
+               </li>
+               <li>
+                 <a href="#"> Criar nova página</a>
+               </li>
+              <li>
+                 <a href="#"> Sair </a>
+              </li>
+
+
+
+
+           </ul>
+       </nav>
+       <!-- /#sidebar-wrapper -->
+
+       <!-- Page Content -->
+       <div id="page-content-wrapper">
+           <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+               <span class="hamb-top"></span>
+         <span class="hamb-middle"></span>
+       <span class="hamb-bottom"></span>
+           </button>
+
+       </div>
+       <!-- /#page-content-wrapper -->
+
+   </div>
+   <!-- /#wrapper -->
