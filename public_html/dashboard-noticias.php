29d15d48777b003b32a05667e9ca2ef661db92a6
“Pedro <pedrohenriquebrandao@aol.com>
2017-10-04 02:39:01 -0300

    RefreshDasboard ADM

--- public_html/dashboard-noticias.php
+++ public_html/dashboard-noticias.php
@@ -36,25 +36,105 @@
   <div class="row">
 
     <div class="col-md-offset-3 col-md-6">
-      <h2>Gerenciar Notícias</h2>
+      <h2>Gerenciar Notícias Publicadas</h2>
     </div>
-
 
     <div class="col-md-offset-3 col-md-6">
-      <h3>Notícias atuais</h3>
-      <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed porttitor lacus. Cras in quam aliquam mi auctor blandit.
-        Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
-      <li class="list-group-item">Notícia 1<span style="color:#e22424;padding-left:545px" class="glyphicon glyphicon-remove "></span></li>
-      <li class="list-group-item">Notícia 2<span style="color:#e22424;padding-left:545px" class="glyphicon glyphicon-remove"></span></li>
-      <li class="list-group-item">Notícia 3<span style="color:#e22424;padding-left:545px" class="glyphicon glyphicon-remove"></span></li>
 
-      </ul>
+      <h3>Notícias</h3>
+
+      <div class="row">
+
+        <div>
+
+          <table class="table table-hover">
+
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Descrição</th>
+                <th>Data de publicação</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Duis sed porttitor arcu. Pellentesque.</td>
+                <td>Sed mattis vel velit eget Sed mattis vel velit eget </td>
+                <td>01/09/2017</td>
+                <td><button type="submit" class="btn btn-danger">Remover</button></button></td>
+              </tr>
+
+              <tr>
+                <td>Fusce vel leo quis metus</td>
+                <td> Pellentesque habitant morbi tristique senectu</td>
+                <td>02/05/2017</td>
+                <td><button type="submit" class="btn btn-danger">Remover</button></button></td>
+              </tr>
+
+              <tr>
+                <td>Praesent pellentesque eu purus quis.</td>
+                <td>Fusce cursus nisi id orci</td>
+                <td>05/10/2017</td>
+                <td><button type="submit" class="btn btn-danger">Remover</button></button></td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+
+        </div>
+
+        <!-- /.col-lg-12 -->
+      </div>
+
       <br>
       <button class="btn btn-warning col-md-offset-3 col-md-6" type="button" data-toggle="modal" data-target="#myModal1">
-        Cadastrar Notícia
+        Cadastrar Nova Notícia
       </button>
+
+      <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Cadastrar Nova Notícia</h4>
+            </div>
+            <div class="modal-body text-justify">
+              <form>
+                <div class="form-group">
+                  <label>Título</label>
+                  <input type="text" class="form-control" id="cargo">
+                </div>
+                <div class="form-group">
+                  <label>Descrição</label>
+                  <input type="text" class="form-control" id="descricao">
+                </div>
+                <div class="form-group">
+                  <label>Data de Publicação</label>
+                  <input type="date" class="form-control" id="data">
+                </div>
+                <div class="form-group">
+                  <label>Enviar Imagem</label>
+                  <input type="file" id="file1" class="custom-file-input">
+                  <span class="custom-file-control"></span>
+                </div>                 
+                <button type="submit" class="btn btn-primary">Enviar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
   </div>
+  </div>
+
+
+
 
   <!-- jQuery -->
   <script src="js/jquery.js"></script>
@@ -63,7 +143,11 @@
   <script src="js/bootstrap.min.js"></script>
   <!-- navBarscript -->
   <script src="js/navbarADM.js">
-
+    < script >
+      $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+      }) <
+      />
 
   </script>
   <br><br><br>
