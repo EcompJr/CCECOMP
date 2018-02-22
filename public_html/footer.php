3dcb93bfd8b5185f9dcdff68d1132fb1d1ea7485
Bruno Vogel <bugvogel@gmail.com>
2018-02-22 19:24:02 -0300

    adjust footer responsive

--- public_html/footer.php
+++ public_html/footer.php
@@ -3,10 +3,10 @@
         <footer style="background-color:#4d6aa0;" >
 
             <div style="height:300px;background-color:#4d6aa0;bottom:0;width:100%;position:relative" class="row">
-                <div  class="col-lg-offset-1 col-lg-3">
+                <div  class="col-lg-offset-1 col-xs-12 col-sm-3 col-md-3 col-lg-3">
                     <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <a target="_blank" href="http://www.ecompjr.com.br/"><img height="35" width='35'  style="border-radius:20px;"  src='../images/logoEcompJr.png'/> </a>Desenvolvido por EcompJr<br></p>
                 </div>
-                <div class="col-lg-3">
+                <div style="position:realtive;left:20px"class="col-lg-3 col-xs-12 col-sm-3 col-md-3 ">
                     <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"><b style="color:white">Colegiado do Curso de Engenharia de Computação</b> 
                     LABOTEC III - SALA I8, UEFS 
                     Av. Transnordestina, S/N 
@@ -15,7 +15,7 @@
                     Feira de Santana/BA 
                     <a target="_blank"  href="https://www.google.com/maps/place/Labotec+3+-+Laborat%C3%B3rios+de+Engenharia+de+Computa%C3%A7%C3%A3o,+UEFS/@-12.207112,-38.973881,14z/data=!4m5!3m4!1s0x0:0xf06781118830e1ef!8m2!3d-12.1981876!4d-38.9718854?hl=pt-BR"><i class="	glyphicon glyphicon-map-marker"></i>Localização</a></p>
                 </div>
-                <div class="col-lg-3">
+                <div style="position:realtive;left:20px"class="col-lg-3 col-xs-12 col-sm-3 col-md-3 ">
                      <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"><a style="color:white" target="_blank" href="http://www.ba.gov.br/">Portal do Governo</a> | <a target="_blank" style="color:white"href="http://www.ouvidoria.ba.gov.br/">Ouvidoria Geral</a> |  <a style="color:white" target="_blank" href="http://www.ba.gov.br/modules/conteudo/conteudo.php?conteudo=18">Acesso à Informação</a> |  <a style="color:white" target="_blank" href="http://www.uefs.br/">UEFS</a> </p>
                 </div>
 
