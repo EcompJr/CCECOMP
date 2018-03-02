30fe9112aef1868aceaf60de7164859fb70e25eb
Bruno Vogel <bugvogel@gmail.com>
2018-03-02 15:05:29 -0300

    resize logo Ecompjr footer

--- public_html/footer.php
+++ public_html/footer.php
@@ -4,7 +4,7 @@
 
             <div style="height:300px;background-color:#002f87;bottom:0;width:100%;position:relative" class="row">
                 <div  class="col-lg-offset-1 col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                    <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <a target="_blank" href="http://www.ecompjr.com.br/"><img height="35" width='35'  style="border-radius:20px;"  src='../images/logoEcompJr.png'/> </a>Desenvolvido por EcompJr<br></p>
+                    <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <a target="_blank" href="http://www.ecompjr.com.br/"><img height="55" width='55'  style="border-radius:20px;"  src='../images/logoEcompJr.png'/> </a>Desenvolvido por EcompJr<br></p>
                 </div>
                 <div style="position:realtive;left:20px"class="col-lg-3 col-xs-12 col-sm-3 col-md-3 ">
                     <p style="color:white;font-size:15px;position:relative;top:40px;font-family:'Lato', sans-serif"><b style="color:white">Colegiado do Curso de Engenharia de Computação</b> 
